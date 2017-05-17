cb334d281df315b59f9413a59be1289a7314cb68
Hayden <hayden.field@devinit.org>
2017-05-17 15:29:13 +0100

    Set a URL to download the humanitarian CSV from
    and fix a copypaste error

--- src/load_remote_data.py
+++ src/load_remote_data.py
@@ -10,7 +10,7 @@ remote_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..
 
 # URLs at which data can be found
 csv_url_summary_stats = 'http://dashboard.iatistandard.org/summary_stats.csv'
-csv_url_humanitarian_stats = 'http://dashboard.iatistandard.org/humanitarian.csv'
+csv_url_humanitarian_stats = 'http://dev.dashboard.iatistandard.org/humanitarian.csv'
 
 
 with open(os.path.join(remote_data_path, 'summary_stats.csv'), 'wb') as f:
@@ -31,11 +31,10 @@ with open(os.path.join(remote_data_path, 'humanitarian.csv'), 'wb') as f:
 	response = requests.get(csv_url_humanitarian_stats)
 
 	if not response.ok:
-		print('There was a problem loading the Summary Statistics data')
+		print('There was a problem loading the Humanitarian Statistics data')
 
 	# TODO: Add error handling - file writing
 	f.write(response.text.encode('utf-8'))
-
 
 
 # TODO: Add mention of __main__ and main()
