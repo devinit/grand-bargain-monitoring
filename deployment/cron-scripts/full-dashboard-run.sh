dece2d1a269823088ea66a6d7996963b1c3d6617
Hayden <hayden.field@devinit.org>
2017-05-18 14:07:50 +0100

    Specify some commands to run via cron

--- deployment/cron-scripts/full-dashboard-run.sh
+++ deployment/cron-scripts/full-dashboard-run.sh
@@ -2,16 +2,26 @@
 
 echo "Updating the Data Snapshot"
 cd /home/stats/gbm-IATI-Stats/data
-# update git and output logs to file
+git checkout automatic > /home/stats/logs/$(date +\%Y\%m\%d)-snapshot.log 2>&1
+git reset --hard > /home/stats/logs/$(date +\%Y\%m\%d)-snapshot.log 2>&1
+git clean -df > /home/stats/logs/$(date +\%Y\%m\%d)-snapshot.log 2>&1
+git pull --ff-only > /home/stats/logs/$(date +\%Y\%m\%d)-snapshot.log 2>&1
 
 echo "Running IATI Stats"
 cd /home/stats/gbm-IATI-Stats
 source pyenv/bin/activate
-# run relevant script and output logs to file
+# run script to generate stats
+./git_dashboard.sh > /home/stats/logs/$(date +\%Y\%m\%d)-stats.log 2>&1
 deactivate
 
 echo "Running IATI Dashboard"
 cd /home/dash/gbm-IATI-Dashboard
 source pyenv/bin/activate
-# run relevant script and output logs to file
+# run script to generate CSVs and such
+./git.sh > /home/dashboard/logs/$(date +\%Y\%m\%d)-dashboard.log 2>&1
 deactivate
+
+echo "Copying data to Scorecard"
+# copy relevant generated CSVs to scorecard repo
+
+echo "Completed data update"
