2a0b49b5ee1ef352191f6ed2072bd956abd16e17
dalepotter <dalepotter@gmail.com>
2017-05-18 11:23:19 +0100

    Add additional sys.path
    This seems to prevent relative import errors in deploying to Apache

--- wsgi.py
+++ wsgi.py
@@ -6,6 +6,7 @@ import sys
 import logging
 logging.basicConfig(stream=sys.stderr)
 sys.path.insert(0,"/var/www/grand-bargain-monitoring/")
+sys.path.insert(0,"/var/www/grand-bargain-monitoring/src/")
 
 from src.app import app as application
 application.secret_key = 'Add your secret key'
