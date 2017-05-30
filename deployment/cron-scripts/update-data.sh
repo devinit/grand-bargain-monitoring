d938b1a6cc78ab8a9f17623baa5cb5423a21e2d6
Hayden <hayden.field@devinit.org>
2017-05-30 15:15:59 +0100

    Update script to copy summary_stats to gbm

--- deployment/cron-scripts/update-data.sh
+++ deployment/cron-scripts/update-data.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 
 echo "Copying data to Scorecard"
+
+# ensure we're not copying an empty file
+num_lines=`cat /home/numbergen/gbm-IATI-Dashboard/web/summary_stats.csv | wc -l`
+if [ $num_lines -lt 2 ]
+then
+    echo "Not enough data to copy"
+    exit 1
+fi
+
 # copy relevant generated CSVs to scorecard repo
+cp /home/numbergen/gbm-IATI-Dashboard/web/summary_stats.csv /var/www/grand-bargain-monitoring/data/remote/summary_stats.csv
 
 echo "Updated Scorecard data"
