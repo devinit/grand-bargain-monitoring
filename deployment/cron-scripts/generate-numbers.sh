48533f757557b7139fa7407b089787067a1b9380
Hayden <hayden.field@devinit.org>
2017-05-30 13:45:11 +0100

    Change filepaths in cron script

--- deployment/cron-scripts/generate-numbers.sh
+++ deployment/cron-scripts/generate-numbers.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 
 echo "Updating the Data Snapshot"
-cd /home/stats/gbm-IATI-Stats/data
-git checkout automatic > /home/stats/logs/$(date +\%Y\%m\%d)-snapshot.log 2>&1
-git reset --hard > /home/stats/logs/$(date +\%Y\%m\%d)-snapshot.log 2>&1
-git clean -df > /home/stats/logs/$(date +\%Y\%m\%d)-snapshot.log 2>&1
-git pull --ff-only > /home/stats/logs/$(date +\%Y\%m\%d)-snapshot.log 2>&1
+cd /home/numbergen/gbm-IATI-Stats/data
+git checkout automatic > /home/numbergen/logs/$(date +\%Y\%m\%d)-snapshot.log 2>&1
+git reset --hard > /home/numbergen/logs/$(date +\%Y\%m\%d)-snapshot.log 2>&1
+git clean -df > /home/numbergen/logs/$(date +\%Y\%m\%d)-snapshot.log 2>&1
+git pull --ff-only > /home/numbergen/logs/$(date +\%Y\%m\%d)-snapshot.log 2>&1
 
 echo "Running IATI Stats"
-cd /home/stats/gbm-IATI-Stats
+cd /home/numbergen/gbm-IATI-Stats
 source pyenv/bin/activate
 # run script to generate stats
-./git_dashboard.sh > /home/stats/logs/$(date +\%Y\%m\%d)-stats.log 2>&1
+./git_dashboard.sh > /home/numbergen/logs/$(date +\%Y\%m\%d)-stats.log 2>&1
 deactivate
 
 echo "Running IATI Dashboard"
-cd /home/dash/gbm-IATI-Dashboard
+cd /home/numbergen/gbm-IATI-Dashboard
 source pyenv/bin/activate
 # run script to generate CSVs and such
-./git.sh > /home/dashboard/logs/$(date +\%Y\%m\%d)-dashboard.log 2>&1
+./git.sh > /home/numbergen/logs/$(date +\%Y\%m\%d)-dashboard.log 2>&1
 deactivate
 
 echo "Completed data update"
