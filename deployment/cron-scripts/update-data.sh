2d824dd179af3628e20c471a3ffe62fee9105fa9
Hayden <hayden.field@devinit.org>
2017-06-07 10:01:18 +0100

    Increase required number of lines in CSV to 500
    At present there are 542 lines in the file, so 541 publishers.
    Should the number drop below 500, something is wrong and the data is likely bad.

--- deployment/cron-scripts/update-data.sh
+++ deployment/cron-scripts/update-data.sh
@@ -4,7 +4,7 @@ echo "Copying summary_stats data to Scorecard"
 
 # ensure we're not copying an empty file
 num_lines=`cat /home/numbergen/gbm-IATI-Dashboard/web/summary_stats.csv | wc -l`
-if [ $num_lines -lt 400 ]
+if [ $num_lines -lt 500 ]
 then
     echo "Not enough summary_stats data to copy"
     exit 1
@@ -19,7 +19,7 @@ echo "Copying humanitarian data to Scorecard"
 
 # ensure we're not copying an empty file
 num_lines=`cat /home/numbergen/gbm-IATI-Dashboard/web/humanitarian.csv | wc -l`
-if [ $num_lines -lt 400 ]
+if [ $num_lines -lt 500 ]
 then
     echo "Not enough humanitarian data to copy"
     exit 1
